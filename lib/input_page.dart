e50e9a1904233c0ccd43a4d7a68b83a733ba20f6
evaldasdoda <55666891+evaldasdoda@users.noreply.github.com>
2019-10-08 12:39:47 +0300

    Commit

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,10 +19,16 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: new ReusableCard(colour: Color(0xff1d1e33)),
+                  child: new ReusableCard(
+                    colour: Color(0xff1d1e33),
+                    cardChild: new IconContent(),
+                  ),
                 ),
                 Expanded(
-                  child: new ReusableCard(colour: Color(0xff1d1e33)),
+                  child: new ReusableCard(
+                    colour: Color(0xff1d1e33),
+                    cardChild: new IconContent(),
+                  ),
                 ),
               ],
             ),
@@ -47,13 +54,48 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  const IconContent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //IconContent({});
+    Icons gender;
+    Text genderText;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          FontAwesomeIcons.mars,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xff8d8e98),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
         color: colour,
